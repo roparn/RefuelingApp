9f907562aa55a44fafdc2cab5b7bae6cd2b0e69a
roparn <robert.parn@gmx.com>
2014-05-20 00:47:06 +0300

    Migrated to MVC

---
+++ src/controller/MainViewController.java
@@ -0,0 +1,195 @@
+package controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.awt.event.KeyEvent;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.List;
+
+import javax.swing.JFileChooser;
+import javax.swing.KeyStroke;
+import javax.swing.RowFilter;
+import javax.swing.table.TableRowSorter;
+
+import models.FuelEntry;
+import models.FuelEntryTableModel;
+import view.MainView;
+import dao.FileDao;
+
+public class MainViewController implements ItemListener {
+
+	private MainView view;
+	private FuelEntryTableModel tableModel;
+	private List<FuelEntry> fuelEntries;
+	private TableRowSorter<FuelEntryTableModel> sorter;
+	private RowFilter<FuelEntryTableModel, Object> fuelTypeFilter;
+	private RowFilter<FuelEntryTableModel, Object> fuelDateFilter;
+	protected Object fuelNameFilterText;
+	protected Date filterMonthText;
+	
+	public MainViewController(MainView view){
+		this.view = view;
+	}
+	public void control(){
+		// Load data file to model elements
+		getData(FileDao.DEFAULT_FILE_LOCATION);
+		// Create table model from list elements
+		tableModel = new FuelEntryTableModel(fuelEntries);
+		// Init GUI components with data and control options
+		initMenuBar();
+		initFilteringComponents();
+		initTable();
+		
+		
+		view.getReloadDataButton().addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				new MainViewController(new MainView()).control();
+				view.dispose();
+			}
+		});
+	}
+	private void initTable(){
+        // Create custom sorter, to sort table columns
+		sorter = new TableRowSorter<FuelEntryTableModel>(tableModel);
+		// This comparator sorts double values of fuel price and amount
+		Comparator<Double> doubleComparator = new Comparator<Double>() {
+			@Override
+			public int compare(Double d1, Double d2) {
+				return Double.compare(d1, d2);
+			}
+		};
+		// This comparator sorts dates column, because they need to be in
+		// custom dd.MM.yyyy format, it needed to be string, which means we need to
+		// reparse it back to date and compare
+		Comparator<String> dateComparator = new Comparator<String>() {
+			@Override
+			public int compare(String s1, String s2) {
+				Date d1,d2;
+				try {
+					d1 = new SimpleDateFormat("dd.MM.yyyy").parse(s1);
+					d2 = new SimpleDateFormat("dd.MM.yyyy").parse(s2);
+				} catch (ParseException e) {
+					e.printStackTrace();
+					return 0;
+				}
+				return d1.compareTo(d2);
+			}
+		};
+		sorter.setComparator(1, doubleComparator);
+		sorter.setComparator(2, doubleComparator);
+		sorter.setComparator(3, dateComparator);
+		
+		view.getTable().setModel(tableModel);
+		view.getTable().setRowSorter(sorter);
+	}
+	private void initMenuBar(){
+		// Open file dialog
+		view.getOpenMenuItem().addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent event) {
+                JFileChooser fileOpen = new JFileChooser();
+                fileOpen.showDialog(view.getPanel(), "Open file");
+                getData(fileOpen.getSelectedFile().getPath());
+			}
+		});
+		// Exit app
+		view.getExitMenuItem().addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		view.getExitMenuItem().setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_W, ActionEvent.CTRL_MASK));
+		
+		// Show statusbar checkbox
+		view.getShowStatusBar().setState(true);
+		// Is visible
+		view.getShowStatusBar().addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent event) {
+				if (view.getStatusbar().isVisible()) {
+					view.getStatusbar().setVisible(false);
+				} else {
+					view.getStatusbar().setVisible(true);
+				}
+			}
+		});
+	}
+	private void initFilteringComponents(){
+		// Lets create a new list from fuelnames and add a combobox
+		// TODO: needs improvement, quick and dirty workaround ATM
+		List<String> fuelTypes = new ArrayList<String>();
+		List<String> refuelingDates = new ArrayList<String>();
+		for (FuelEntry item : fuelEntries){
+			if (!fuelTypes.contains(item.getFuelName())){
+				fuelTypes.add(item.getFuelName());
+				view.getComboBox().addItem(item.getFuelName());
+			}
+			if (!refuelingDates.contains(new SimpleDateFormat("MMM").format(item.getRefuelingDate()))){
+				refuelingDates.add(new SimpleDateFormat("MMM").format(item.getRefuelingDate()));
+				view.getComboBox2().addItem(new SimpleDateFormat("MMM").format(item.getRefuelingDate()));
+			}
+		}
+		view.getComboBox().addItemListener(this);
+		view.getComboBox2().addItemListener(this);
+		
+		fuelTypeFilter = new RowFilter<FuelEntryTableModel, Object>(){
+			@SuppressWarnings("rawtypes")
+			@Override
+			public boolean include(RowFilter.Entry entry) {
+				if (fuelNameFilterText.equals("all"))
+					return true;
+				return entry.getValue(0).equals(fuelNameFilterText.toString());
+			}
+		};
+		fuelDateFilter = new RowFilter<FuelEntryTableModel, Object>(){
+			@Override
+			public boolean include(Entry<? extends FuelEntryTableModel, ? extends Object> entry) {
+				try {
+					String d1 = new SimpleDateFormat("MMM").
+							format(new SimpleDateFormat("dd.MM.yyyy").parse((String) entry.getValue(3)));
+					return d1.equals(new SimpleDateFormat("MMM").format(filterMonthText));
+				} catch (ParseException e) {
+					return true;
+				}
+			}
+		};
+	}
+	public void getData(String fileLocation){
+		FileDao fd = new FileDao();
+		try {
+			fuelEntries = fd.getFuelEntries(fd.openFile(fileLocation));
+			view.getStatusbar().setText("File parsed succesfully");
+		}catch (ParseException e) {
+			view.getStatusbar().setText("Error parsing values from file");
+		}catch (FileNotFoundException e){
+			view.getStatusbar().setText("File not found");
+		}catch (IOException e) {
+			view.getStatusbar().setText("File IO error");
+			e.printStackTrace();
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+	}
+	@Override
+	public void itemStateChanged(ItemEvent event) {
+		if (event.getStateChange() == ItemEvent.SELECTED) {
+			try {
+				filterMonthText = new SimpleDateFormat("MMM").parse(event.getItem().toString());
+				sorter.setRowFilter(fuelDateFilter);
+			} catch (ParseException e1) {
+				fuelNameFilterText = (String)event.getItem();
+				sorter.setRowFilter(fuelTypeFilter);
+			}
+        }
+	}
+}
---
+++ src/view/MainView.java
@@ -0,0 +1,173 @@
+package view;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.event.KeyEvent;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.border.EtchedBorder;
+
+public class MainView extends JFrame {
+	private static final long serialVersionUID = 1L;
+	
+	private JPanel panel;
+	private JTable table;
+	private final FlowLayout layout = new FlowLayout();
+	private JLabel statusbar;
+	private JLabel sumLabel;
+	private JButton reloadDataButton;
+	private JMenuItem openMenuItem;
+	private JMenuItem exitMenuItem;
+	private JMenu viewMenu;
+	private JCheckBoxMenuItem showStatusBar;
+	private JComboBox<Object> comboBox;
+	private JComboBox<Object> comboBox2;
+
+	public JCheckBoxMenuItem getShowStatusBar() {
+		return showStatusBar;
+	}
+	public void setShowStatusBar(JCheckBoxMenuItem showStatusBar) {
+		this.showStatusBar = showStatusBar;
+	}
+	public JComboBox<Object> getComboBox() {
+		return comboBox;
+	}
+	public void setComboBox(JComboBox<Object> comboBox) {
+		this.comboBox = comboBox;
+	}
+	public JComboBox<Object> getComboBox2() {
+		return comboBox2;
+	}
+	public void setComboBox2(JComboBox<Object> comboBox2) {
+		this.comboBox2 = comboBox2;
+	}
+	public JMenuItem getExitMenuItem() {
+		return exitMenuItem;
+	}
+	public void setExitMenuItem(JMenuItem exitMenuItem) {
+		this.exitMenuItem = exitMenuItem;
+	}
+	public JMenuItem getOpenMenuItem() {
+		return openMenuItem;
+	}
+	public void setOpenMenuItem(JMenuItem openMenuItem) {
+		this.openMenuItem = openMenuItem;
+	}
+	public JButton getReloadDataButton() {
+		return reloadDataButton;
+	}
+	public void setReloadData(JButton reloadData) {
+		this.reloadDataButton = reloadData;
+	}
+	public JPanel getPanel() {
+		return panel;
+	}
+	public void setPanel(JPanel panel) {
+		this.panel = panel;
+	}
+	public JTable getTable() {
+		return table;
+	}
+	public void setTable(JTable table) {
+		this.table = table;
+	}
+	public FlowLayout getLayout() {
+		return layout;
+	}
+	public JLabel getStatusbar() {
+		return statusbar;
+	}
+	public void setStatusbar(JLabel statusbar) {
+		this.statusbar = statusbar;
+	}
+	public JLabel getSumLabel() {
+		return sumLabel;
+	}
+	public void setSumLabel(JLabel sumLabel) {
+		this.sumLabel = sumLabel;
+	}
+	
+	public MainView(){
+		initFrame();
+		initPanel();
+		initStatusbar();
+		initMenuBar();
+		initFilteringComponents();
+		initTable();
+	}
+	private void initTable(){
+        table = new JTable();
+        table.setPreferredScrollableViewportSize(new Dimension(450, 300));
+        JScrollPane scrollPane = new JScrollPane(table);
+        panel.add(scrollPane);
+	}
+	private void initFilteringComponents(){
+		
+		comboBox = new JComboBox<Object>(new String[]{"all"});
+		comboBox2 = new JComboBox<Object> (new String[]{"all"});
+		panel.add(comboBox);
+		panel.add(comboBox2);
+	}
+	private void initFrame(){
+		setVisible(true);
+		setTitle("Refuel App");
+		setSize(500, 500);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	private void initStatusbar() {
+		// Add statusbar as a label
+        statusbar = new JLabel("Ready");
+        statusbar.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.RAISED));
+        add(statusbar, BorderLayout.SOUTH);
+	}
+	private void initMenuBar() {
+		// Menu bar
+		JMenuBar menubar = new JMenuBar();
+		// "File" menu
+		JMenu fileMenu = new JMenu("File");
+		fileMenu.setMnemonic(KeyEvent.VK_F);
+		// "View" menu
+		viewMenu = new JMenu("View");
+		// Menu items
+		// Open file
+		openMenuItem = new JMenuItem("Open");
+		openMenuItem.setToolTipText("Open file");
+		// Exit app
+		exitMenuItem = new JMenuItem("Exit");
+		exitMenuItem.setToolTipText("Exit application");
+		// Add "View" menu item
+		showStatusBar = new JCheckBoxMenuItem("Show StatuBar");
+		showStatusBar.setState(true);
+		// Add menu items to menu
+		fileMenu.add(openMenuItem);
+		fileMenu.add(exitMenuItem);
+		viewMenu.add(showStatusBar);
+		// Add menu to menubar
+		menubar.add(fileMenu);
+		menubar.add(viewMenu);
+		setJMenuBar(menubar);
+	}
+	private void initPanel(){
+		// Initialize panel
+		panel = new JPanel();
+		panel.setLayout(layout);
+        // Adding load default file button
+        reloadDataButton = new JButton("Reload");
+        reloadDataButton.setToolTipText("Reload data");
+		panel.add(reloadDataButton);
+		add(panel);
+	}
+}
